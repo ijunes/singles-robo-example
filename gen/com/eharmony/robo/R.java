df2361fbf33ba45cc206ed6136e053911ee6ead5
jkang <junukang@eharmony.com>
2014-05-12 17:59:38 -0700

    working demo

--- gen/com/eharmony/robo/R.java
+++ gen/com/eharmony/robo/R.java
@@ -12,8 +12,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int progress=0x7f020001;
+    }
+    public static final class id {
+        public static final int classNameText=0x7f050000;
+        public static final int expandableListView=0x7f050008;
+        public static final int failureList=0x7f050003;
+        public static final int gallerylayout=0x7f050004;
+        public static final int methodNameText=0x7f050001;
+        public static final int progressBar=0x7f050005;
+        public static final int shortDetailsText=0x7f050002;
+        public static final int startButton=0x7f050007;
+        public static final int suiteNameTextView=0x7f050006;
+    }
+    public static final class layout {
+        public static final int junittestdetails=0x7f030000;
+        public static final int junittestrunner=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
 }
--- src/com/eharmony/robo/Runner.java
+++ src/com/eharmony/robo/Runner.java
@@ -1,19 +1,21 @@
 package com.eharmony.robo;
 
-import com.eharmony.robo.tests.rq.TestRQ;
-import com.eharmony.test.TestDashboard;
-import com.eharmony.test.TestSignUp;
+import com.eharmony.robo.test.TestRQ;
+import com.eharmony.robo.test.TestDashboard;
+import com.eharmony.robo.test.TestSignUp;
 
+import android.net.wifi.WifiManager;
 import android.test.InstrumentationTestRunner;
 import android.test.InstrumentationTestSuite;
 import junit.framework.TestSuite;
 
 public class Runner extends InstrumentationTestRunner {
 private InstrumentationTestSuite mainSuite = new InstrumentationTestSuite(this);;
-    @Override
+    
+@Override
     public TestSuite getAllTests(){
     	InstrumentationTestSuite suite = new InstrumentationTestSuite(this);
-        suite.addTestSuite(TestSignUp.class);
+        //suite.addTestSuite(TestSignUp.class);
         suite.addTestSuite(TestRQ.class);
         mainSuite.addTest(suite);
         return mainSuite;
@@ -31,5 +33,7 @@ private InstrumentationTestSuite mainSuite = new InstrumentationTestSuite(this);
     public ClassLoader getLoader() {
         return Runner.class.getClassLoader();
     }
+    
+   
 
 }
